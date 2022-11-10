c25717e80315181f17037cd2e1c69fae2a21b912
FernandoAndrada <fernandojcandrada@gmail.com>
2022-11-10 15:56:46 -0300

    Criar [M3S07}-Ex8-aniversario

---
+++ index.js
@@ -0,0 +1,24 @@
+var birthYear = 1966;
+var currentYear = 0;
+var i = -1;
+
+console.log("do{ } while( )")
+
+do{
+    i++;
+    console.log(currentYear = birthYear + i);
+
+}while(!(currentYear == 2022))
+
+console.log("===========")
+console.log("while( )")
+
+
+var birthY = 1966;
+var curretY = 0;
+var j = 0;
+
+while (curretY < 2022){
+    console.log(curretY = birthY + j);
+    j++;
+}
